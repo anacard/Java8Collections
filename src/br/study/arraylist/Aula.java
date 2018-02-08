2af90d8b249f3c45b0ae0cd95ac4ef1176fd20df
Ana Paula Rodrigues Correa <ana.card.rodrigues@gmail.com>
2018-02-07 22:45:50 -0200

    initial commit

---
+++ src/br/study/arraylist/Aula.java
@@ -0,0 +1,27 @@
+package br.study.arraylist;
+
+public class Aula implements Comparable<Aula>{
+	private String nome;
+	private int tempo;
+	
+	public Aula(String nome, int tempo) {
+		super();
+		this.nome = nome;
+		this.tempo = tempo;
+	}
+	public int getTempo() {
+		return tempo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	@Override
+	public String toString() {
+		return "Aula [nome=" + nome + ", tempo=" + tempo + "]";
+	}
+
+	@Override
+	public int compareTo(Aula aula) {
+		return this.compareTo(aula);
+	}
+}
